c0ca0a159c19c8c3187cc4e4d4c1f1bef94a7f87
vbhv4GitHub <ravi9406126068@gmail.com>
2023-02-19 14:26:37 +0530

    chore(seo): :zap: remove google analytics for project

--- components/SEO/Analytics.tsx
+++ components/SEO/Analytics.tsx
@@ -2,34 +2,15 @@ import Script from 'next/script';
 
 const Analytics = () => {
   return (
-    <>
-      <Script
-        strategy='lazyOnload'
-        id='googleAnalytics'
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}`}
-      />
-
-      <Script strategy='lazyOnload' id='googleAnalyticsScript'>
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-            });
-                `}
-      </Script>
-
-      <Script strategy='lazyOnload' id='microsoftClarityScript'>
-        {`
+    <Script strategy="lazyOnload" id="microsoftClarityScript">
+      {`
            (function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", '${process.env.NEXT_PUBLIC_MICROSOFT_CLARITY_TRACKING_CODE}');
                 `}
-      </Script>
-    </>
+    </Script>
   );
 };
 
