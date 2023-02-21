738fd7083b93bbf2303c5330bd51e401b3a4e630
vbhv4GitHub <ravi9406126068@gmail.com>
2023-02-22 01:10:24 +0530

    chore(build): :wrench: remove fullhuman postcss option from postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    ['cssnano', isProduction ? {} : false],
-    ['autoprefixer', isProduction ? {} : false],
-    [
-      '@fullhuman/postcss-purgecss',
-      isProduction
-        ? {
-            // the paths to all template files
-            content: ['./pages/**/*', './components/**/*'],
-            // function used to extract class names from the templates
-            defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-          }
-        : false,
-    ],
-  ],
+  plugins: ['tailwindcss', ['cssnano', isProduction ? {} : false], ['autoprefixer', isProduction ? {} : false]],
 };
